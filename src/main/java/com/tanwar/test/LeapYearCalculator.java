1ddc9ebe707c9a8c86b7fb987c3edc9e9658feb7
Amardeep Tanwar <tanwar.amar@gmail.com>
2020-05-25 23:26:40 +0400

    All years divisible by 4 but not by 100 ARE leap years - refactor

--- src/main/java/com/tanwar/test/LeapYearCalculator.java
+++ src/main/java/com/tanwar/test/LeapYearCalculator.java
@@ -1,19 +1,15 @@
 package com.tanwar.test;
 
 public class LeapYearCalculator {
-	public boolean isLeapYear(long number) {
+	public boolean isLeapYear(long year) {
 
 		// All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
-		if(number % 400 == 0) {
+		if(year % 400 == 0) {
 			return true;
 		}
 		
-		//All years divisible by 100 but not by 400 are NOT leap years
-		else if (number % 100 == 0 && number % 400 != 0) {
-			return false;
-		}
 		// All years divisible by 4 but not by 100 ARE leap years
-		else if (number % 4 == 0 && number % 100 != 0) {
+		else if (year % 4 == 0 && year % 100 != 0) {
 			return true;
 		}
 
