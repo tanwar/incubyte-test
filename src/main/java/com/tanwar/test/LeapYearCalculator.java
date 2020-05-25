745b3968192377822405c93120d182fea0fbf364
Amardeep Tanwar <tanwar.amar@gmail.com>
2020-05-25 23:01:23 +0400

    impl class added but need to start on the acceptance criteria

---
+++ src/main/java/com/tanwar/test/LeapYearCalculator.java
@@ -0,0 +1,14 @@
+package com.tanwar.test;
+
+/**
+ *
+ */
+public class LeapYearCalculator 
+{
+    public boolean isLeapYear(long number)
+    {
+    	
+    	return false;
+    	
+    }
+}
--- src/test/java/com/tanwar/test/LeapYearCalculatorTest.java
+++ src/test/java/com/tanwar/test/LeapYearCalculatorTest.java
@@ -1,13 +1,21 @@
 package com.tanwar.test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearCalculatorTest {
 	
+	private LeapYearCalculator leapYearCalculator;
+	
+	@Before
+	public void setup() {
+		leapYearCalculator = new LeapYearCalculator();
+	}
+	
 	@Test
 	public void shouldVerifyLeapYear() {
-		Assert.fail("no leap year implementation found!");
+		Assert.assertTrue(leapYearCalculator.isLeapYear(1600));
 	}
 	
 	
