2126e5b3f11e4211e81610d3665fffff8142eed5
Amardeep Tanwar <tanwar.amar@gmail.com>
2020-05-25 23:04:18 +0400

    All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year)

--- src/main/java/com/tanwar/test/LeapYearCalculator.java
+++ src/main/java/com/tanwar/test/LeapYearCalculator.java
@@ -1,14 +1,13 @@
 package com.tanwar.test;
 
-/**
- *
- */
-public class LeapYearCalculator 
-{
-    public boolean isLeapYear(long number)
-    {
-    	
-    	return false;
-    	
-    }
+public class LeapYearCalculator {
+	public boolean isLeapYear(long number) {
+
+		if (number % 400 == 0) {
+			return true;
+		}
+
+		return false;
+
+	}
 }
--- src/test/java/com/tanwar/test/LeapYearCalculatorTest.java
+++ src/test/java/com/tanwar/test/LeapYearCalculatorTest.java
@@ -5,18 +5,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class LeapYearCalculatorTest {
-	
+
 	private LeapYearCalculator leapYearCalculator;
-	
+
 	@Before
 	public void setup() {
 		leapYearCalculator = new LeapYearCalculator();
 	}
-	
+
 	@Test
 	public void shouldVerifyLeapYear() {
 		Assert.assertTrue(leapYearCalculator.isLeapYear(1600));
 	}
-	
-	
+
 }
