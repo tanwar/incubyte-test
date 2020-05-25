aeec924d73e54990d392ab41b02e63e92165b493
Amardeep Tanwar <tanwar.amar@gmail.com>
2020-05-25 22:57:17 +0400

    first commit - no impl

---
+++ src/test/java/com/tanwar/test/LeapYearCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.tanwar.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LeapYearCalculatorTest {
+	
+	@Test
+	public void shouldVerifyLeapYear() {
+		Assert.fail("no leap year implementation found!");
+	}
+	
+	
+}
